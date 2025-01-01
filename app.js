305a6b775a526b33c4a7286f9b0a2b633589987e
Kevin <kevinsteven0513@gmail.com>
2025-01-01 18:32:16 -0500

    Dark Mode y pequeños cambios

---
+++ app.js
@@ -0,0 +1,16 @@
+function mostrarValores() {
+    const valorInput = document.getElementById('valor').value;
+    console.log(valorInput)
+}
+
+
+// Obtenemos el valor del switch -->
+document.getElementById('switch').addEventListener('change', (e) => {
+    console.log(e.target.checked);
+    const bodyStyle = document.querySelector('body');
+    const headerTitle = document.getElementById('headerTitle');
+    bodyStyle.style.transition = 'all 0.5s ease';
+    headerTitle.style.transition = 'all 0.5s ease';
+    bodyStyle.classList.toggle('dark-body');
+    headerTitle.classList.toggle('dark-header-title');
+})
