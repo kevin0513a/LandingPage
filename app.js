374c64ff8d2c5a53f92e346bd538f6ce4e5f4358
Kevin <kevinsteven0513@gmail.com>
2025-01-06 17:30:56 -0500

    Cambios generales en master

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ document.getElementById('switch').addEventListener('change', (e) => {
         console.log('El switch está activado');
     } else console.log('El switch está desactivado');
     const bodyStyle = document.querySelector('body');
-    const headerTitle = document.getElementById('headerTitle');
+    const headerTitle = document.getElementById('headerTitle'); 
     bodyStyle.style.transition = 'all 0.5s ease';
     headerTitle.style.transition = 'all 0.5s ease';
     bodyStyle.classList.toggle('dark-body');
     headerTitle.classList.toggle('dark-header-title');
+
+    document.querySelectorAll('.button-card').forEach(el => {
+        el.classList.toggle('dark-button');
+    });
 });
+
